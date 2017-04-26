ca4b4605ad81040de2be538482e68b39918cef58
Andy Ho <89andy@gmail.com>
2017-04-26 23:14:49 +0800

    header mockup done

--- src/containers/ChannelHeader.js
+++ src/containers/ChannelHeader.js
@@ -7,13 +7,12 @@ class ChannelHeader extends Component {
       <header className="navbar navbar-default navbar-static-top" id="channel-header">
         <nav className="container">
           <div className="navbar-header">
+            <a className="navbar-brand"><span className="glyphicon glyphicon-menu-left" /></a>
             <a className="navbar-brand" href="#">MusicBox</a>
           </div>
           <div className="nav">
             <ul className="nav navbar-nav navbar-right">
-              <li><a href="#">History</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Playlist</a></li>
+              <li><a href="#"><span className="glyphicon glyphicon-list" /></a></li>
             </ul>
           </div>
         </nav>
